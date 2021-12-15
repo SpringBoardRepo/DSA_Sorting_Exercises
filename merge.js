0ce05a727de341e8e245be2a387b6511a557bd98
poorvarathore05 <poorvarathore05@gmail.com>
2021-12-14 16:41:23 -0800

    implemented mergeSort

---
+++ merge.js
@@ -0,0 +1,39 @@
+function merge(arr1, arr2) {
+
+    let i = 0;
+    let j = 0;
+    const result = [];
+
+    while (i < arr1.length && j < arr2.length) {
+
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        }
+        else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+    return result;
+}
+
+function mergeSort(array) {
+
+    if (array.length <= 1) return array;
+    let mid = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, mid));
+    let right = mergeSort(array.slice(mid));
+
+    return merge(left, right)
+}
+
+module.exports = { merge, mergeSort };
